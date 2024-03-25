fb79b3b0df419112e8838ec4be70e3b6a12d6a64
Salazar, Axel (as323n) <as323n@att.com>
2024-03-25 13:02:59 -0500

    Add jar file

--- src/main/java/com/carfax/controller/CarfaxController.java
+++ src/main/java/com/carfax/controller/CarfaxController.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +20,6 @@ public class CarfaxController {
   Logger log = LoggerFactory.getLogger(CarfaxController.class);
   private final CarfaxService carfaxService;
 
-  @Autowired
   public CarfaxController(final CarfaxService carfaxService) {
     this.carfaxService = carfaxService;
   }
